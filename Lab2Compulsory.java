676ecc22a435335fc3dd88c8810890227913ee46
StefanMunteanu18 <stef.munteanu18@gmail.com>
2022-02-28 17:41:49 +0200

    Commit final de laborator

---
+++ Lab2Compulsory.java
@@ -0,0 +1,6 @@
+public class Lab2Compulsory {
+    public static void main(String []args) {
+        Event object = new Event("aaa", 10, 1,2);
+        System.out.println(object);
+    }
+}
---
+++ Room.java
@@ -0,0 +1,23 @@
+public class Room {
+    private String name;
+    private int capacity;
+    private String type;
+
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
